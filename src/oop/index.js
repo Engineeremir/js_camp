b05d0dcc67fdfdb173d62398fda784af39b598a7
emirhan.kalem320 <emirhan.kalem320@gmail.com>
2021-10-23 19:28:03 +0300

    oop

---
+++ src/oop/index.js
@@ -0,0 +1,32 @@
+class Customer {
+    constructor(id, customerNumber) {
+        this.id = id,
+            this.customerNumber = customerNumber
+    }
+}
+
+let customer = new Customer(1, "12345");
+
+//prototyping
+customer.name = "emirhan"
+console.log(customer.name)
+
+//static
+Customer.sth = "something"
+console.log(Customer.sth)
+
+console.log(customer.customerNumber)
+
+class IndividualCustomer extends Customer {
+    constructor(firstName, id, customerNumber) {
+        super(id, customerNumber),
+            this.firstName = firstName
+    }
+}
+
+class CorporateCustomer extends Customer {
+    constructor(companyName, id, customerNumber) {
+        super(id, customerNumber),
+            this.companyName = companyName
+    }
+}
