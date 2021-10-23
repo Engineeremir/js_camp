807f42e4b0148f2bda9f634373298efaac0d78d6
emirhan.kalem320 <emirhan.kalem320@gmail.com>
2021-10-23 17:34:51 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,20 @@
+console.log("emir")
+let dolarDun = "9.2"
+
+{
+    let dolarDun = "2.2"
+
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+    //euroDun = 2 => euroDun is read only
+console.log(euroDun)
+
+let krediler = ["Konut Kredisi", "Emlak Konut Kredisi", "Komu Konut Kredisi"]
+
+for (let i = 0; i < krediler.length; i++) {
+    console.log(krediler[i])
+
+}
